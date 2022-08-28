2dcb65271553bf678df9d073478fbb847b2ef1e2
MitranK16 <105253161+MitranK16@users.noreply.github.com>
2022-08-28 14:52:07 -0400

    Create File.java

---
+++ File.java
@@ -0,0 +1,26 @@
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+ 
+public class File {
+ 
+    public static void main(String[] args)
+        throws IOException
+    {
+        // Assigning the content of the file
+        String text
+            = "Hello";
+
+        Path fileName = Path.of(
+            "/Users/mayanksolanki/Desktop/javafile.docx");
+ 
+
+        Files.writeString(fileName, text);
+ 
+
+        String file_content = Files.readString(fileName);
+
+        System.out.println(file_content);
+    }
+}
